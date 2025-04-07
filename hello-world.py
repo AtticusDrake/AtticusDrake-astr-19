97ed236ea33f5f7f7b4fb686997dc884b0ee37dd
AtticusDrake <adrake1@ucsc.edu>
2025-04-07 16:18:12 -0700

    Added hello world program

---
+++ hello-world.py
@@ -0,0 +1,16 @@
+#This program will write
+#Hello World
+
+#This function tells the computer to print hello world
+def PrintHelloWorld():
+	print("Hello World")
+
+
+#this defines our main function for our  program
+def main():
+	PrintHelloWorld()
+
+
+#when we run the program this executes first
+if __name__=='__main__':
+	main()
