76b7c689a325f3b8be2658bd69e7a3dee1f8939c
AtticusDrake <adrake1@ucsc.edu>
2025-04-07 16:34:04 -0700

    Added name program

---
+++ name.py
@@ -0,0 +1,9 @@
+#This program will print my name and information about my favorite movie and food
+def PrintName():
+	print("My name is Atticus.  My favorite movie is It's A Wonderful Life.  My favorite food is Pad See Yew.")
+
+def main():
+	PrintName()
+
+if __name__=='__main__':
+	main()
