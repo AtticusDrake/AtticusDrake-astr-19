91493d69079ef7cbf81a343de880e5133a67446d
AtticusDrake <adrake1@ucsc.edu>
2025-04-07 17:43:32 -0700

    Added session 2 prompt program

---
+++ session-2-prompt.py
@@ -0,0 +1,12 @@
+import numpy as np
+x = 6
+y = 4
+i = 8
+
+print("x = 5, y = 4, i = 8")
+
+print ("This is a floating point number. x+y = ", x+y)
+
+print ("This is a floating point number. x-y = ", x-y)
+
+print ("This is a floating point number. y*i = ", y*i)
